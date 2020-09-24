a644577e9d641156f3770ebaa42972e5e53d5cdb
praveen3073 <pop3073@gmail.com>
2020-09-24 12:42:21 +0530

    Added: UC2 Line Equality Check Revised

--- src/Line.java
+++ src/Line.java
@@ -11,10 +11,4 @@ public class Line {
 		this.y1 = y1;
 		this.y2 = y2;
 	}
-	@Override
-	public boolean equals(Object obj)
-	{
-		Line lineObj = (Line) obj;
-		return lineObj.x1==this.x1 && lineObj.x2==this.x2 && lineObj.y1==this.y1 && lineObj.y2==this.y2;
-	}
 }
