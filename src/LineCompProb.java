7e9e5d94fccad97bc7feb4c456aec2e14ab3c166
praveen3073 <pop3073@gmail.com>
2020-09-24 11:58:26 +0530

    Added: UC1 Line Length Calculation

--- src/LineCompProb.java
+++ src/LineCompProb.java
@@ -3,5 +3,12 @@ import java.util.*;
 public class LineCompProb {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		calcLineLength();
+	}
+	public static void calcLineLength()
+	{
+		Line line = new Line(4,3,2,1);
+		double lineLength = Math.sqrt(Math.pow(line.x2-line.x1,2)+Math.pow(line.y2-line.y1,2));
+		System.out.println("The length of line is: "+lineLength);
 	}
 }
